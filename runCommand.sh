35e3e48373c573f87d80f7d495b26f9430b97514
asher-williams-techolution <asher.williams@techolution.com>
2024-02-18 00:59:21 -0500

    Docker run fixed

--- runCommand.sh
+++ runCommand.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+# Build the Docker image
 docker build -t watch-defect-app .
-docker stop watch-defect-app
-docker rm  watch-defect-app
-docker run -d --gpus all --ipc=host -p 80:8080 --name watch-defect-app watch-defect-app --restart=always
-docker logs -f --since=1m watch-defect-app
+
+# Stop and remove the previous container if it exists
+docker ps | grep "watch-defect-app" && docker stop watch-defect-app
+docker ps -a | grep "watch-defect-app" && docker rm watch-defect-app
+
+# Run the Docker container with GPU support and restart policy
+docker run -d --gpus all --ipc=host -p 80:8080 --restart=always --name watch-defect-app watch-defect-app
+
+# Follow the logs with a timestamp since the last minute
+docker logs -f --since 1m watch-defect-app
